4bc56a4d1fc6f22445040866303c257072dcdd07
not7cd <norbert@not7cd.net>
2021-12-04 14:07:31 +0100

    Add views

--- meta/urls.py
+++ meta/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from meta import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("login/", views.login),
+    path("auth/", views.auth, name="auth"),
 ]
